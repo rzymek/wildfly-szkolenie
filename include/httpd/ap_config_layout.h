b60161cf040983da0a64298035e809679fd153b2
balancer <rzymek@gmail.com>
2015-09-23 11:17:11 +0200

    clean initial

---
+++ include/httpd/ap_config_layout.h
@@ -0,0 +1,64 @@
+/* Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @file  ap_config_layout.h
+ * @brief Apache Config Layout
+ */
+
+#ifndef AP_CONFIG_LAYOUT_H
+#define AP_CONFIG_LAYOUT_H
+
+/* Configured Apache directory layout */
+#define DEFAULT_PREFIX "/opt/jboss/httpd/httpd"
+#define DEFAULT_EXP_EXEC_PREFIX "/opt/jboss/httpd"
+#define DEFAULT_REL_EXEC_PREFIX "/opt/jboss/httpd"
+#define DEFAULT_EXP_BINDIR "/opt/jboss/httpd/bin"
+#define DEFAULT_REL_BINDIR "/opt/jboss/httpd/bin"
+#define DEFAULT_EXP_SBINDIR "/opt/jboss/httpd/sbin"
+#define DEFAULT_REL_SBINDIR "/opt/jboss/httpd/sbin"
+#define DEFAULT_EXP_LIBEXECDIR "/opt/jboss/httpd/lib/httpd/modules"
+#define DEFAULT_REL_LIBEXECDIR "/opt/jboss/httpd/lib/httpd/modules"
+#define DEFAULT_EXP_MANDIR "/opt/jboss/httpd/man"
+#define DEFAULT_REL_MANDIR "/opt/jboss/httpd/man"
+#define DEFAULT_EXP_SYSCONFDIR "/opt/jboss/httpd/httpd/conf"
+#define DEFAULT_REL_SYSCONFDIR "conf"
+#define DEFAULT_EXP_DATADIR "/opt/jboss/httpd/htdocs"
+#define DEFAULT_REL_DATADIR "/opt/jboss/httpd/htdocs"
+#define DEFAULT_EXP_INSTALLBUILDDIR "/opt/jboss/httpd/htdocs/build"
+#define DEFAULT_REL_INSTALLBUILDDIR "/opt/jboss/httpd/htdocs/build"
+#define DEFAULT_EXP_ERRORDIR "/opt/jboss/httpd/htdocs/error"
+#define DEFAULT_REL_ERRORDIR "/opt/jboss/httpd/htdocs/error"
+#define DEFAULT_EXP_ICONSDIR "/opt/jboss/httpd/htdocs/icons"
+#define DEFAULT_REL_ICONSDIR "/opt/jboss/httpd/htdocs/icons"
+#define DEFAULT_EXP_HTDOCSDIR "/opt/jboss/httpd/htdocs/htdocs"
+#define DEFAULT_REL_HTDOCSDIR "/opt/jboss/httpd/htdocs/htdocs"
+#define DEFAULT_EXP_MANUALDIR "/opt/jboss/httpd/htdocs/manual"
+#define DEFAULT_REL_MANUALDIR "/opt/jboss/httpd/htdocs/manual"
+#define DEFAULT_EXP_CGIDIR "/opt/jboss/httpd/htdocs/cgi-bin"
+#define DEFAULT_REL_CGIDIR "/opt/jboss/httpd/htdocs/cgi-bin"
+#define DEFAULT_EXP_INCLUDEDIR "/opt/jboss/httpd/include/httpd"
+#define DEFAULT_REL_INCLUDEDIR "/opt/jboss/httpd/include/httpd"
+#define DEFAULT_EXP_LOCALSTATEDIR "/opt/jboss/httpd/httpd"
+#define DEFAULT_REL_LOCALSTATEDIR ""
+#define DEFAULT_EXP_RUNTIMEDIR "/opt/jboss/httpd/httpd/logs"
+#define DEFAULT_REL_RUNTIMEDIR "logs"
+#define DEFAULT_EXP_LOGFILEDIR "/opt/jboss/httpd/httpd/logs"
+#define DEFAULT_REL_LOGFILEDIR "logs"
+#define DEFAULT_EXP_PROXYCACHEDIR "/opt/jboss/httpd/httpd/proxy"
+#define DEFAULT_REL_PROXYCACHEDIR "proxy"
+
+#endif /* AP_CONFIG_LAYOUT_H */
